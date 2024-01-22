112664a33b15d2829d8afb946fc060a0e390de5d
seu-username <seu-email>
2024-01-21 21:12:20 -0300

    att do prtifolio

--- arquivo.js
+++ arquivo.js
@@ -1,22 +1,9 @@
-let swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
-
-let Header = document.getElementById('header')
+let header = document.getElementById('header')
 
 window.addEventListener('scroll', () => {
   if (window.scrollY >= 200) {
-    Headers.style.background = '#191919'
+    header.style.background = '#191919'
   } else {
-    Headers.style.background = 'transparent'
+    header.style.background = 'transparent'
   }
 })
